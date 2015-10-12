ddee9e068a5f81ca62ab14de3856796b4bfd55cb
Ben Levinsky <ben@levinsky.org>
2015-10-11 19:07:09 -0700

    and should have added all out of date files

--- SSUMobile_android/app/src/main/java/com/app/ssumobile/ssumobile_android/CalendarActivity.java
+++ SSUMobile_android/app/src/main/java/com/app/ssumobile/ssumobile_android/CalendarActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -34,4 +36,19 @@ public class CalendarActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /** Called when the user clicks the Send button */
+    public void testConnection(View view) {
+        // Do something in response to button
+        boolean connected = false;
+        String message = "still no cnxn :(";
+
+        // if condition works then say so!
+        if (connected){
+            message = "got cnxn :)";
+        }
+        Toast.makeText(getBaseContext(), "Your answer is correct!", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
