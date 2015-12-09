ab4295a873e573b6317812261a53a4bd5d0a7a90
Ben Levinsky <ben@levinsky.org>
2015-12-09 15:47:10 -0800

    has progress bar

--- SSUMobile_android/app/src/main/java/com/app/ssumobile/ssumobile_android/activity/DepartmentsActivity.java
+++ SSUMobile_android/app/src/main/java/com/app/ssumobile/ssumobile_android/activity/DepartmentsActivity.java
@@ -1,10 +1,14 @@
 package com.app.ssumobile.ssumobile_android.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +16,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import com.app.ssumobile.ssumobile_android.R;
 import com.app.ssumobile.ssumobile_android.models.BuildingModel;
 import com.app.ssumobile.ssumobile_android.models.DepartmentModel;
@@ -66,6 +72,15 @@ public class DepartmentsActivity extends AppCompatActivity {
             }
         });
 
+        // get data into listview in the background
+        new ProgressTask(DepartmentsActivity.this).execute();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         // Enable Search Filter for search logic
         inputSearch.addTextChangedListener(new TextWatcher() {
             @Override
@@ -83,69 +98,65 @@ public class DepartmentsActivity extends AppCompatActivity {
 
             }
         });
-    }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Thread runner = new Thread(new Runnable(){
-            public void run()  {
-                try {
-                    sendGet("https://moonlight.cs.sonoma.edu/ssumobile/1_0/directory.py");
-                } catch (Throwable t) {
-                    System.out.println(t.getCause());
-                }
-            }
-        });
-        runner.start();
-
-        try {
-            runner.join();
-            adapter.notifyDataSetChanged(); // update cards
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        Thread runner = new Thread(new Runnable(){
+//            public void run()  {
+//                try {
+//                    sendGet("https://moonlight.cs.sonoma.edu/ssumobile/1_0/directory.py");
+//                } catch (Throwable t) {
+//                    System.out.println(t.getCause());
+//                }
+//            }
+//        });
+//        runner.start();
+//
+//        try {
+//            runner.join();
+//            adapter.notifyDataSetChanged(); // update cards
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         // Set the building name for each department
-        Thread runner2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i = 0; i < buildingList.size(); ++i) {
-                    for (int j = 0; j < contactsList.size(); ++j) {
-                        if ( buildingList.get(i).id.equals( contactsList.get(j).building ) )
-                            contactsList.get(j).buildingName = buildingList.get(i).name;
-                    }
-                }
-            }
-        });
-        runner2.start();
-
-        try {
-            runner2.join();
-            adapter.notifyDataSetChanged(); // update cards
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        Thread runner3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for( int i = 0; i < facStaffList1.size(); ++i ){
-                    for( int k = 0; k < contactsList.size(); ++k ){
-                        if(contactsList.get(k).id.equals(facStaffList1.get(i).department ))
-                            contactsList.get(k).getFacStaffList().add( facStaffList1.get(i) );
-                    }
-                }
-            }
-        });
-        runner3.start();
-
-        try {
-            runner3.join();
-            adapter.notifyDataSetChanged(); // update cards
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        Thread runner2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for(int i = 0; i < buildingList.size(); ++i) {
+//                    for (int j = 0; j < contactsList.size(); ++j) {
+//                        if ( buildingList.get(i).id.equals( contactsList.get(j).building ) )
+//                            contactsList.get(j).buildingName = buildingList.get(i).name;
+//                    }
+//                }
+//            }
+//        });
+//        runner2.start();
+//
+//        try {
+//            runner2.join();
+//            adapter.notifyDataSetChanged(); // update cards
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        Thread runner3 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for( int i = 0; i < facStaffList1.size(); ++i ){
+//                    for( int k = 0; k < contactsList.size(); ++k ){
+//                        if(contactsList.get(k).id.equals(facStaffList1.get(i).department ))
+//                            contactsList.get(k).getFacStaffList().add( facStaffList1.get(i) );
+//                    }
+//                }
+//            }
+//        });
+//        runner3.start();
+//
+//        try {
+//            runner3.join();
+//            adapter.notifyDataSetChanged(); // update cards
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
@@ -161,7 +172,7 @@ public class DepartmentsActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.Faculty_Staff_Directory:
                 Thread runner = new Thread(new Runnable() {
                     @Override
@@ -175,26 +186,26 @@ public class DepartmentsActivity extends AppCompatActivity {
                 return true;
 
             case R.id.Buildings_Directory:
-               Thread Brunner = new Thread(new Runnable() {
-                   @Override
-                   public void run() {
-                       Intent Bintent = new Intent(DepartmentsActivity.this, BuildingsActivity.class);
-                       finish();
-                       startActivity(Bintent);
-                   }
-               });
-               Brunner.start();
+                Thread Brunner = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent Bintent = new Intent(DepartmentsActivity.this, BuildingsActivity.class);
+                        finish();
+                        startActivity(Bintent);
+                    }
+                });
+                Brunner.start();
                 return true;
 
             case R.id.Schools_Directory:
                 Thread Srunner = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Intent Sintent = new Intent(DepartmentsActivity.this, SchoolsActivity.class);
-                    finish();
-                    startActivity(Sintent);
-                }
-            });
+                    @Override
+                    public void run() {
+                        Intent Sintent = new Intent(DepartmentsActivity.this, SchoolsActivity.class);
+                        finish();
+                        startActivity(Sintent);
+                    }
+                });
                 Srunner.start();
                 return true;
 
@@ -222,14 +233,14 @@ public class DepartmentsActivity extends AppCompatActivity {
         String inputLine;
         StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null) {response.append(inputLine);}
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
         in.close();
 
         body = response.toString();
-        String test = body.substring(0, 15);
-        System.out.println("in sendGet()");
-        System.out.println(test);
-        parseOutEvents();
+
+     //   parseOutEvents();
     }
 
 
@@ -237,36 +248,24 @@ public class DepartmentsActivity extends AppCompatActivity {
     private void parseOutEvents() throws org.json.JSONException {
         System.out.println("in parseOutEvents()");
 
-        Thread runner = new Thread(new Runnable() {
-            @Override
-            public void run() {
-               try {
-                   JSONObject myjson = new JSONObject(body);
-                   JSONArray the_json_array = myjson.getJSONArray("Department");
-                   JSONArray building_json_array = myjson.getJSONArray("Building");
-                   JSONArray facStaff_json_array = myjson.getJSONArray("Person");
-                   for (int i = 0; i < the_json_array.length(); i++) {
-                       contactsList.add(convertDeptJSONtoModel(the_json_array.getJSONObject(i)));
-                       adapter.notifyDataSetChanged(); // update cards
-                   }
-                   for (int i = 0; i < building_json_array.length(); i++) {
-                       buildingList.add(convertBuildJSONtoModel(building_json_array.getJSONObject(i)));
-                       adapter.notifyDataSetChanged(); // update cards
-                   }
-                   for (int i = 0; i < facStaff_json_array.length(); i++){
-                       facStaffList1.add(convertPersonJSONtoModel(facStaff_json_array.getJSONObject(i)));
-                       adapter.notifyDataSetChanged(); // update cards
-                   }
-               }catch( JSONException e){
-                   throw new RuntimeException(e);
-               }
-            }
-        });
-        runner.start();
+
+        JSONObject myjson = new JSONObject(body);
+        JSONArray the_json_array = myjson.getJSONArray("Department");
+        JSONArray building_json_array = myjson.getJSONArray("Building");
+        JSONArray facStaff_json_array = myjson.getJSONArray("Person");
+        for (int i = 0; i < the_json_array.length(); i++) {
+            contactsList.add(convertDeptJSONtoModel(the_json_array.getJSONObject(i)));
+        }
+        for (int i = 0; i < building_json_array.length(); i++) {
+            buildingList.add(convertBuildJSONtoModel(building_json_array.getJSONObject(i)));
+        }
+        for (int i = 0; i < facStaff_json_array.length(); i++) {
+            facStaffList1.add(convertPersonJSONtoModel(facStaff_json_array.getJSONObject(i)));
+        }
     }
 
     // get attributes of event string into an event
-    public DepartmentModel convertDeptJSONtoModel(JSONObject s) throws org.json.JSONException{
+    public DepartmentModel convertDeptJSONtoModel(JSONObject s) throws org.json.JSONException {
         DepartmentModel currentContact = new DepartmentModel();
 
         currentContact.ac = s.getString("ac");
@@ -286,7 +285,7 @@ public class DepartmentsActivity extends AppCompatActivity {
         return currentContact;
     }
 
-    public BuildingModel convertBuildJSONtoModel(JSONObject s) throws org.json.JSONException{
+    public BuildingModel convertBuildJSONtoModel(JSONObject s) throws org.json.JSONException {
         BuildingModel currentContact = new BuildingModel();
 
         currentContact.Created = s.getString("Created");
@@ -299,7 +298,7 @@ public class DepartmentsActivity extends AppCompatActivity {
     }
 
     // get attributes of event string into an event
-    public FacStaffModel convertPersonJSONtoModel(JSONObject s) throws org.json.JSONException{
+    public FacStaffModel convertPersonJSONtoModel(JSONObject s) throws org.json.JSONException {
         FacStaffModel currentContact = new FacStaffModel();
 
         currentContact.office = s.getString("office");
@@ -316,5 +315,87 @@ public class DepartmentsActivity extends AppCompatActivity {
         currentContact.email = s.getString("email");
 
         return currentContact;
+    }
+
+
+    private class ProgressTask extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog dialog;
+        private DepartmentsActivity activity;
+
+        public ProgressTask(DepartmentsActivity activity) {
+            this.activity = activity;
+            context = activity;
+            dialog = new ProgressDialog(context);
+        }
+
+        /** progress dialog to show user that the backup is processing. */
+
+        /**
+         * application context.
+         */
+        private Context context;
+
+        protected void onPreExecute() {
+            this.dialog.setMessage("Progress start");
+            this.dialog.show();
+        }
+
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            // after downloading and parsing
+            for (int i = 0; i < buildingList.size(); ++i) {
+                for (int j = 0; j < contactsList.size(); ++j) {
+                    if (buildingList.get(i).id.equals(contactsList.get(j).building))
+                        contactsList.get(j).buildingName = buildingList.get(i).name;
+                }
+            }
+            for (int i = 0; i < facStaffList1.size(); ++i) {
+                for (int k = 0; k < contactsList.size(); ++k) {
+                    if (contactsList.get(k).id.equals(facStaffList1.get(i).department))
+                        contactsList.get(k).getFacStaffList().add(facStaffList1.get(i));
+                }
+            }
+            adapter.notifyDataSetChanged(); // update cards
+
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+            if (success) {
+                Toast.makeText(context, "Thanks for waiting!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Error", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+        private void trysendGet(){
+            try {
+                sendGet("https://moonlight.cs.sonoma.edu/ssumobile/1_0/directory.py");
+            } catch (Throwable t) {
+                System.out.println(t.getCause());
+            }
+        }
+
+        private void tryparseOutEvents(){
+            try {
+                parseOutEvents();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        protected Boolean doInBackground(final String... args) {
+
+            this.dialog.setMessage("Downloading data...");
+            trysendGet();
+
+            tryparseOutEvents();
+
+            return true;
+        }
+
     }
 }
